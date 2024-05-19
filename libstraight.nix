d200791ace1239f37c7e4825302e096a48ef38fc
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2024-05-18 17:57:22 -0700

    modifications to make this usable with (use-package :straight t)

--- libstraight.nix
+++ libstraight.nix
@@ -1,90 +1,113 @@
-{ abortOnNotFound ? true,
-  lib, stdenv, epkgs, emacs, writeScript }:
-
-let
+{
+  abortOnNotFound ? true,
+  lib,
+  stdenv,
+  epkgs,
+  emacs,
+  writeScript,
+}: let
   inherit (builtins) filter trace;
   inherit (lib) concatMapStringsSep escapeShellArgs importJSON flatten unique optionalString warn;
 
-  expandDependencies = packages:
-    let
-      withDeps = p:
-        map (x:
-          if x == null then [ ] else
-          [ x ] ++ withDeps x.propagatedBuildInputs
-        ) (flatten p);
-   in (unique (filter (d: d ? ename) (flatten (withDeps packages))));
+  expandDependencies = packages: let
+    withDeps = p:
+      map (
+        x:
+          if x == null
+          then []
+          else [x] ++ withDeps x.propagatedBuildInputs
+      ) (flatten p);
+  in (unique (filter (d: d ? ename) (flatten (withDeps packages))));
 
-  install = repo: packages:
-    let
-      installPkg = repo: pkg: (''
-        REPO=${repo}
-        psrc=(${pkg}/share/emacs/*/*/${pkg.ename}*)
-        if [[ ! -d $psrc ]]; then
-          elpa_path=(${pkg}/share/emacs/site-lisp/elpa/*)
-          if [[ -d $elpa_path ]]; then
-            ln -snf $elpa_path $REPO/${pkg.ename}
-          else
-            ln -snf ${pkg}/share/emacs/site-lisp $REPO/${pkg.ename}
-          fi
+  install = repo: packages: let
+    installPkg = repo: pkg: ''
+      REPO=${repo}
+      psrc=(${pkg}/share/emacs/*/*/${pkg.ename}*)
+      if [[ ! -d $psrc ]]; then
+        elpa_path=(${pkg}/share/emacs/site-lisp/elpa/*)
+        if [[ -d $elpa_path ]]; then
+          ln -snf $elpa_path $REPO/${pkg.ename}
         else
-          ln -snf $psrc $REPO/${pkg.ename}
+          ln -snf ${pkg}/share/emacs/site-lisp $REPO/${pkg.ename}
         fi
-        ${optionalString ((pkg.src ? meta) && (pkg.src.meta ? homepage)) ''
-          if [[ ! -d $REPO/${baseNameOf pkg.src.meta.homepage} ]]; then
-            ln -snf $psrc $REPO/${baseNameOf pkg.src.meta.homepage}
-          fi
-        ''}
-      '');
-    in  writeScript "install-repo" ''
+      else
+        ln -snf $psrc $REPO/${pkg.ename}
+      fi
+      ${optionalString ((pkg.src ? meta) && (pkg.src.meta ? homepage)) ''
+        if [[ ! -d $REPO/${baseNameOf pkg.src.meta.homepage} ]]; then
+          ln -snf $psrc $REPO/${baseNameOf pkg.src.meta.homepage}
+        fi
+      ''}
+    '';
+  in
+    writeScript "install-repo" ''
       mkdir -p ${repo}
       ${(concatMapStringsSep "\n" (installPkg repo) (expandDependencies packages))}
     '';
 
-  parsePackagesJSON = json:
-    let
-      list = importJSON json;
-    in map (x:
-      if epkgs ? "${x}" then epkgs.${x}
-      else if abortOnNotFound then abort "Package not available: ${x}"
-      else (warn "Package not available: ${x}") null) list;
+  parsePackagesJSON = json: let
+    list = importJSON json;
+  in
+    map (x:
+      if epkgs ? "${x}"
+      then epkgs.${x}
+      else if abortOnNotFound
+      then abort "Package not available: ${x}"
+      else (warn "Package not available: ${x}") null)
+    list;
 
-  packagesJSON = { emacsInitFile, emacsLoadFiles, emacsArgs }: stdenv.mkDerivation {
-    name = "emacs-straight-packages.json";
-    buildInputs = [ emacs ];
-    buildPhase = ":";
-    installPhase = ''
-      runHook preInstall
-      emacs -q      \
-            --batch \
-            --directory=${epkgs.straight}/share/emacs/site-lisp \
-            --load=${./setup.el} \
-            ${concatMapStringsSep "\n" (f: "--load=${f}") emacsLoadFiles} \
-            --eval="(nix-straight-get-used-packages \"${emacsInitFile}\" \"$out\")" \
-            ${escapeShellArgs emacsArgs}
-      runHook postInstall
-    '';
-  };
+  packagesJSON = {
+    emacsInitFile,
+    emacsLoadFiles,
+    emacsArgs,
+  }:
+    stdenv.mkDerivation {
+      name = "emacs-straight-packages.json";
+      dontUnpack = true;
+      buildInputs = [emacs];
+      buildPhase = ":";
+      installPhase = ''
+        runHook preInstall
+        emacs -q      \
+              --batch \
+              --directory=${epkgs.straight}/share/emacs/site-lisp \
+              --load=${./setup.el} \
+              ${concatMapStringsSep " " (f: "--load=${f}") emacsLoadFiles} \
+              --eval="(nix-straight-get-used-packages \"${emacsInitFile}\" \"$out\")" \
+              ${escapeShellArgs emacsArgs}
+        runHook postInstall
+      '';
+    };
 
-  emacsEnv = { emacsInitFile, emacsLoadFiles, emacsArgs }: { packages, straightDir }: stdenv.mkDerivation {
-    name = "straight-emacs-env";
-    buildPhase = ":";
-    buildInputs = [ emacs ];
-    installPhase = ''
-      runHook preInstall
+  emacsEnv = {
+    emacsInitFile,
+    emacsLoadFiles,
+    emacsArgs,
+  }: {
+    packages,
+    straightDir,
+  }:
+    stdenv.mkDerivation {
+      name = "straight-emacs-env";
+      dontUnpack = true;
+      buildPhase = ":";
+      buildInputs = [emacs];
+      installPhase = ''
+        runHook preInstall
 
-      mkdir -p $out
-      ${(install "${straightDir}/repos" packages)}
-      emacs -q \
-            --batch \
-            --directory=${epkgs.straight}/share/emacs/site-lisp \
-            --load=${./setup.el} \
-            ${concatMapStringsSep "\n" (f: "--load=${f}") emacsLoadFiles} \
-            --eval="(nix-straight-build-packages \"${emacsInitFile}\")" ${escapeShellArgs emacsArgs} \
-            || (cat $out/logs/cli.doom.*.error 1>&2 && false) # print a proper stacktrace if things fail
+        mkdir -p $out
+        ${(install "${straightDir}/repos" packages)}
+        emacs -q \
+              --batch \
+              --directory=${epkgs.straight}/share/emacs/site-lisp \
+              --load=${./setup.el} \
+              ${concatMapStringsSep "\n" (f: "--load=${f}") emacsLoadFiles} \
+              --eval="(nix-straight-build-packages \"${emacsInitFile}\")" ${escapeShellArgs emacsArgs} \
+              || (cat $out/logs/cli.doom.*.error 1>&2 && false) # print a proper stacktrace if things fail
 
-      runHook postInstall
-    '';
-  };
+        runHook postInstall
+      '';
+    };
 in {
   inherit install parsePackagesJSON packagesJSON emacsEnv;
 }
--- setup.el
+++ setup.el
@@ -2,45 +2,51 @@
 (require 'json)
 
 (defun nix-straight-get-used-packages (init-file output-file)
-  (let ((nix-straight--packages nil))
-    (advice-add 'straight-use-package
-                :override (lambda (recipe &rest r)
-                            (let ((pkg (if (listp recipe)
-                                           (car recipe)
-                                         recipe)))
-                              (message "[nix-straight.el] Collecting package '%s' from recipe '%s'" pkg recipe)
-                              (add-to-list 'nix-straight--packages pkg))))
+  (let* ((nix-straight--packages nil)
+	 (adv (lambda (type)
+		(lambda (recipe &rest r)
+		  (let ((pkg (if (listp recipe)
+				 (car recipe)
+			       recipe)))
+		    (message "[nix-straight.el] Collecting package '%s' from recipe '%s'" pkg recipe)
+		    (unless (and (equal type 'use-package) (not (member :straight r)))
+		      (add-to-list 'nix-straight--packages pkg))
+		    nil)))))
+    (advice-add 'use-package :override (funcall adv 'use-package))
+    (advice-add 'straight-use-package :override (funcall adv 'straight-use-package))
 
     (load init-file nil nil t)
     (let ((json-pkgs (if (null nix-straight--packages)
-               "[]"
-             (json-encode nix-straight--packages))))
+			 "[]"
+		       (json-encode nix-straight--packages))))
       (write-region json-pkgs nil output-file))
 
     nix-straight--packages))
 
+
 (defun nix-straight-build-packages (init-file)
   (setq straight-default-files-directive '("*" (:exclude "*.elc")))
-  (advice-add 'straight-use-package
-              :around (lambda (orig-fn &rest r)
-                        (message "     [nix-straight.el] Overriding recipe for '%s'" (car r))
-                        (let* ((pkg (car r))
-                               (pkg-name (symbol-name pkg))
-                               (recipe (if (file-exists-p (straight--repos-dir pkg-name))
-                                           (list pkg :local-repo pkg-name :repo pkg-name :type 'git)
-                                         (list pkg :type 'built-in))))
-                          (message "     --> [nix-straight.el] Recipe generated: %s" recipe)
-                          (straight-override-recipe recipe))
-                        (apply orig-fn r)))
+  (let ((adv (lambda (orig-fn &rest r)
+	       (message "     [nix-straight.el] Overriding recipe for '%s'" (car r))
+	       (let* ((pkg (car r))
+		      (pkg-name (symbol-name pkg))
+		      (recipe (if (file-exists-p (straight--repos-dir pkg-name))
+				  (list pkg :local-repo pkg-name :repo pkg-name :type 'git)
+				(list pkg :type 'built-in))))
+		 (message "     --> [nix-straight.el] Recipe generated: %s" recipe)
+		 (straight-override-recipe recipe))
+	       (apply orig-fn r))))
+    (advice-add 'straight-use-package :around adv)
+    (advice-add 'use-package :around adv))
   (load init-file nil nil t))
 
 ;; straight breaks for some reason when
 ;; it tries to build packages in a nativeComp emacs
 (with-eval-after-load "comp"
   (advice-add 'straight--native-compile-file-p
-        :before (lambda (&rest r)
-                    (if (not (boundp 'comp-deferred-compilation-deny-list))
-                        (defvar comp-deferred-compilation-deny-list '())))))
+              :before (lambda (&rest r)
+			(if (not (boundp 'comp-deferred-compilation-deny-list))
+                            (defvar comp-deferred-compilation-deny-list '())))))
 
 (provide 'setup)
 ;;; setup.el ends here
